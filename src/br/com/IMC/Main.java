4daa8e0f7b40fd40b6df3540c4cc08e4cb92bfe4
gabrielvvs1 <87244095+gabrielvvs1@users.noreply.github.com>
2024-09-16 15:56:24 -0300

    Update Main.java
    alteração no println

--- src/br/com/IMC/Main.java
+++ src/br/com/IMC/Main.java
@@ -8,9 +8,9 @@ public class Main {
 		Pessoa objpessoa = new Pessoa ("Gabriel", 75.0 , 1.83);
 		Atleta objatleta = new Atleta ("Fulano", 70.0, 1.75, "Futebol");		
 		
-		
-		System.out.println("O IMC de " + objpessoa.getNome() + " eh de " + objpessoa.calculoIMC());
-		System.out.println("O IMC de " + objatleta.getNome() + " eh de " + objatleta.calculoIMC());
+		System.out.println(String.format("O IMC de %s eh %.2f", objpessoa.getNome(), objpessoa.calculoIMC()));
+        
+        	System.out.println(String.format("O IMC de %s que eh atleta, eh de %.2f", objatleta.getNome(), objatleta.calculoIMC()));
 
 	}
 
